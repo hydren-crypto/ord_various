53438dd5019fbfcb7722c9017f02b2fd50ba7d99
Kevin Sitzes <ksitzes@amperecomputing.com>
2023-02-10 19:18:53 -0500

    chore: add cmdline arg processing, and multi-file

--- inscribe.sh
+++ inscribe.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-file="${1}"
-shift
-fee="${1:=4}"
-shift
+# wrapper around ord to save log files in json to aws
+# simplifies mult-file inscription processing
+
+# perhaps we add functionality to launch in subshells and trigger something
+# when the inscription is confirmed? 
 
 get_unconfirmed_trx(){
     ord wallet transactions | grep -E '\s0$' | awk '{ print $1 }' | uniq
@@ -35,9 +36,8 @@ check_balance(){
     fi
 }
 
-
 fetch_json_log(){
-    aws s3 cp s3://hydren.io/inscribed/${inscribe_log} .
+    aws s3 cp "${aws_s3_uri}"/${inscribe_log} .
 }
 
 prep_json_to_log(){
@@ -51,47 +51,91 @@ close_json_file(){
 }
 
 send_file_to_aws(){  # ORIGINAL_NAME  TARGET_NAME
-    aws s3 cp "${1}" s3://hydren.io/inscribed/${2:=$1}
+    aws s3 cp "${1}" "${aws_s3_uri}"/${2:=$1}
 }
 
 get_aws_url(){
     #potentially add a direct reference for the file
-    aws s3 presign s3://hydren.io/inscribed/${1} --expires-in 604800  #1 week
-}    
+    aws s3 presign "${aws_s3_uri}"/${1} --expires-in 604800  #1 week
+} 
+
+usage(){
+    echo "USAGE: $0 -f [fee rate] -d [description] FILE1 FILE2 ..."
+    echo ""
+    echo " -f   | fee rate [ default: ${fee_rate}]"
+    echo " -d   | description - used for an identifier in the JSON output"
+    echo ""
+    exit 0
+}
 
 tmp_file=tmp_out.txt
 confirm_file=confirmations.txt
 inscribe_log=inscribe_log.json
+fee_rate=4
+aws_s2_uri=s3://hydren.io/inscribed
+
+while [[ $1 =~ ^- ]]; do
+    case $1 in
+
+    file="${1}"
+shift
+        "--fee"|"-f")
+            shift
+            fee_rate=$1
+            ;;
+        "--description"|"-d")
+            shift
+            ord_description=$1
+            ;;
+        "--check"|"-c")
+            CHECKONLY=true
+            ;;
+        *)
+            echo "Unknown option $1"
+            echo; usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [ $# -eq 0 ]; then
+ usage
+fi
 
 mkdir "./done" 2> /dev/null
 
 check_balance
-echo "proceeding with ${file} and a fee rate of ${fee}"
+echo "proceeding with ${cmdline_filename} and a fee rate of ${fee}"
 
-ord wallet inscribe ${file} --fee-rate ${fee} &> $tmp_file
-ord_success=$?
+for cmdline_filename in "$@"; do 
 
-if [[ ${ord_success} -eq 0 ]]; then
-    confirmation=$(cat ${tmp_file}  | jq -r '.commit')
-    inscription=$(cat ${tmp_file} | jq -r '.inscription')
-    inscr_url=https://ordinals.com/inscription/$inscription
-    echo "Confirmation: http://mempool.space/tx/${confirmation}"
-    
-    # check_confirmation ${confirmation}
-    send_file_to_aws "${file}" "${inscription}_${file}" && mv "${file}" ./done/${inscription}_${file}
-    aws_url=$(get_aws_url "${inscription}_${file}")
-    fetch_json_log # download from aws to append
-    prep_json_to_log   
-    cat ${tmp_file} | jq --arg file "$file"  '. + {"filename": $file}' | \
-	    jq --arg fee "$fee" '. + {"fee_rate": $fee}' | \
-	    jq --arg aws_url "$aws_url" '. + {"aws_url": $aws_url}' | \
-	    jq --arg explorer "$inscr_url" '. + {"explorer": $explorer}' >> ${inscribe_log}
-    close_json_file
-    send_file_to_aws "${inscribe_log}" "${inscribe_log}" 
-else
-    echo "Unsuccessful inscription!"
-    echo "$(cat $tmp_file)"
-fi
+    ord wallet inscribe ${cmdline_filename} --fee-rate ${fee_rate} &> $tmp_file
+    ord_success=$?
 
+    if [[ ${ord_success} -eq 0 ]]; then
+        confirmation=$(cat ${tmp_file}  | jq -r '.commit')
+        inscription=$(cat ${tmp_file} | jq -r '.inscription')
+        inscr_url=https://ordinals.com/inscription/$inscription
+        echo "Confirmation: http://mempool.space/tx/${confirmation}"
+        
+        # check_confirmation ${confirmation}
+        send_file_to_aws "${cmdline_filename}" "${inscription}_${cmdline_filename}" && mv "${cmdline_filename}" ./done/${inscription}_${cmdline_filename}
+        aws_url=$(get_aws_url "${inscription}_${cmdline_filename}")
+        fetch_json_log # download from aws to append
+        prep_json_to_log   
+        cat ${tmp_file} | jq --arg file "$cmdline_filename"  '. + {"filename": $cmdline_filename}' | \
+            jq --arg fee_rate "$fee_rate" '. + {"fee_rate": $fee_rate}' | \
+            jq --arg aws_url "$aws_url" '. + {"aws_url": $aws_url}' | \
+            jq --arg explorer "$inscr_url" '. + {"explorer": $explorer}' | \
+            jq --arg description "$ord_description" '. + {"description": $description}' >> ${inscribe_log}
+        close_json_file
+        send_file_to_aws "${inscribe_log}" "${inscribe_log}" 
+    else
+        echo "Unsuccessful inscription!"
+        echo "$(cat $tmp_file)"
+    fi
 
-rm "${tmp_file}"
+    rm "${tmp_file}"
+
+done
