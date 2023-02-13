1173a7b29aebbe931d308308bbb60789ce3859e6
Hydren <hola@hydren.io>
2023-02-13 04:28:40 +0000

    chore: cleanup vars, strip quotes, disable API push

--- ordsea.sh
+++ ordsea.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
-
 # POST ordsea.io/api/create 
 # json: { inscription, proof, price, contact }
 
 # jq '.[] | select(.description | contains("FLOON")) | .inscription' < inscribe_log.json
-
 get_address(){
-    address=$(curl -s  https://ordapi.xyz/inscription/"$1" | jq .address)
+    address=$(curl -s  https://ordapi.xyz/inscription/$1 | jq .address)
+}
+
+get_description(){
+    echo=$1
+    description=$(jq --arg desc "$1" '.[] | select(.inscription == $desc) | .description' $inscribe_log)
 }
 
 inscription_id=$1
-ordsea_create_api="https://ordsea.io/api/create"
+shift
 
-get_address "$inscription_id"
-proof=$(bitcoin-cli signmessage "$address" "$inscription_id 0")
+inscription_stripped=${inscription_id//\"/}
+ordsea_create_api="https://ordsea.io/api/create"
+inscribe_log=inscribe_log.json
+
+get_address $inscription_stripped
+get_description $inscription_stripped
+
+noquote_address=${address//\"/}
+proof=$(bitcoin-cli signmessage "$noquote_address" "$inscription_stripped 0")
 contact="@mrarwyn"
 price="0.42"
 
+echo "ins: $inscription_stripped"
+echo "address-unused: $address"
 echo "proof: $proof"
+echo "description: $description"
 
-curl -d '{"inscription":"$inscription", "proof":"$proof", "price":"$price", "contact":"$contact"}' -H "Content-Type: application/json" -X POST $ordsea_create_api
+#curl -d '{"inscription":"'"$inscription_stripped"'", "proof":"'"$proof"'", "price":"'"$price"'", "contact":"'"$contact"'", "description":""}' -H 'Content-Type: application/json' -X POST $ordsea_create_api
+echo "" 
