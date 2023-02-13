a78e78a2b01347a34f615556940fe61b612bda32
Kevin Sitzes <ksitzes@amperecomputing.com>
2023-02-12 21:11:14 -0500

    fix: get wallet address first

--- ordsea.sh
+++ ordsea.sh
@@ -3,16 +3,19 @@
 # POST ordsea.io/api/create 
 # json: { inscription, proof, price, contact }
 
+# jq '.[] | select(.description | contains("FLOON")) | .inscription' < inscribe_log.json
+
 get_address(){
     address=$(curl -s  https://ordapi.xyz/inscription/"$1" | jq .address)
 }
 
 inscription_id=$1
 ordsea_create_api="https://ordsea.io/api/create"
+get_address
 proof=$(bitcoin-cli signmessage "$address" "$inscription_id 0")
 contact="@mrarwyn"
 price="0.42"
 
-echo "$proof"
+echo "proof: $proof"
 
 curl -d '{"inscription":"$inscription", "proof":"$proof", "price":"$price", "contact":"$contact"}' -H "Content-Type: application/json" -X POST $ordsea_create_api
