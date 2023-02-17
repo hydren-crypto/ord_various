b2351ece3c2a336e3f5f03b09d98e3fd782c5ed0
Hydren <hola@hydren.io>
2023-02-17 18:42:57 +0000

    fix: create_html syntax

--- create_html.sh
+++ create_html.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # create a html file with a gallery listing of all images
+inscribe_log=inscribe_log.json
+html_output=gallery.html
 
 echo "<html>
 <head>
@@ -9,10 +11,13 @@ echo "<html>
 <body>
     <h1>Inscription Gallery</h1>
     <div>
-        $(for explorer in $(jq -r '.[] | .explorer' $inscribe_log); do
-            content_url=$(echo "$explorer" | sed "s@\(https://ordinals.com/inscriptions/\)\(.*\)\(i0\)@\1\2\3/content@g")
-            echo "<a href=\"$explorer_url\"><img src=\"$url\"/>"
+        $(for explorer_url in $(jq -r '.[] | .explorer' $inscribe_log); do
+            content_url=$(echo "$explorer_url" | sed -E 's/\/inscription\//\/content\//g')
+            echo "<a href=\"$explorer_url\"><img src=\"$content_url\"/>"
         done)
     </div>
 </body>
-</html>" > gallery.html
+</html>" > $html_output
+
+aws s3 cp $html_output s3://hydren.io/inscribed/$html_output
+aws cloudfront create-invalidation --distribution-id "$CLOUDFRONT_ID" --paths /inscribed/$html_output
