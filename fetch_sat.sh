220bf39375148c40c57e3d08764203243b3b27c7
Kevin Sitzes <ksitzes@amperecomputing.com>
2023-02-18 17:36:09 -0500

    chore: add inscribe_id to log

--- fetch_sat.sh
+++ fetch_sat.sh
@@ -8,6 +8,7 @@ fetch_sat(){
     # returns: 0 if sat is found, 1 if not
     # output: sat value
     sat=$(curl -s https://ordinals.com/inscription/${inscription} | grep -oP '(?<=<a href=/sat/)[^"]+' | grep -o '[0-9]\{13\}' | uniq 2>/dev/null)
+    inscription_id=$(curl -s https://ordinals.com/inscription/${inscription} | grep -o '<title>Inscription [0-9]\+</title>')
 }
 
 
@@ -15,6 +16,7 @@ tmp_file=tmp_sat_out.json
 inscribe_log=inscribe_log.json
 last_inscription=$(jq -r '.[] | .inscription' $inscribe_log | tail -1)
 
+cp $inscribe_log inscribe_log.bak
 rm $tmp_file 2>/dev/null
 
 echo "[" > $tmp_file
@@ -24,10 +26,14 @@ for inscription in $(jq -r '.[] | .inscription' $inscribe_log); do
     if [ -z "$sat" ]; then
         sat="unknown-or-pending-inscription"
     fi
-    echo "sat: $sat - inscription: $inscription"
-    #jq  --arg inscription "$inscription" --arg sat "$sat" 'group_by(.inscription) | map((.[0] + {sat: (if .[0].inscription == $inscription then $sat else .[0].sat end)}))' $inscribe_log
-    #jq --arg inscription "$inscription" --arg sat "$sat" 'group_by(.inscription) | map(reduce .[] as $x (.; if $x.inscription == $inscription then . + {sat: $sat} else . end))' $inscribe_log
-    jq --arg inscription "$inscription" --arg sat "$sat" '.[] | select(.inscription == $inscription) | . + {sat: $sat}' $inscribe_log >> $tmp_file
+    if [ -z "$inscription_id" ]; then
+        inscription_id="unknown-or-pending-inscription"
+    fi
+
+    echo "sat: $sat - inscription_id: $inscription_id - inscription: $inscription"
+    jq --arg inscription "$inscription" --arg sat "$sat" --arg inscription_id "$inscription_id" \
+        '.[] | select(.inscription == $inscription) | . + {sat: $sat, inscription_id: $inscription_id}' $inscribe_log >> $tmp_file
+
     # Check if $inscription is equal to the last element in the loop
     if [ "$inscription" == "$last_inscription" ]; then
         echo "]" >> $tmp_file
@@ -36,15 +42,13 @@ for inscription in $(jq -r '.[] | .inscription' $inscribe_log); do
         echo "," >> $tmp_file
         continue
     fi
-
-    #jq --arg inscription "$inscription" --arg sat "$sat" 'map(select(.inscription == $inscription) + {sat: $sat}) | unique_by(.inscription)' $inscribe_log >> $tmp_file
  done
 
-echo "input file"
+echo "$inscribe_log"
 cat $inscribe_log | grep -wc "inscription"
 cat $inscribe_log | grep -wc "sat"
 
-
+echo "$tmp_file"
 cat $tmp_file | grep -wc "inscription" 
 cat $tmp_file | grep -wc "sat"
 
@@ -52,7 +56,7 @@ cat $tmp_file | grep -wc "sat"
 # jq '[.[] | group_by(.commit | map(add) | .[]]' $tmp_file > new_file.txt
 jq --slurp '[.[] | group_by(.inscription) | map(reduce .[] as $item ({}; . * $item))]' $tmp_file | jq '.[0]' > new_file.txt
 
-
+echo "new_file.txt"
 cat new_file.txt | grep -wc "inscription"
 cat new_file.txt | grep -wc "sat"
 
