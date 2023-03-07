9c8b58c9296b01b0120412cdca63e7826f1c4c8d
Kevin Sitzes <ksitzes@amperecomputing.com>
2023-03-07 11:59:15 -0500

    chore: cleanup for public consumption

--- inscribe.sh
+++ inscribe.sh
@@ -1,50 +1,33 @@
 #!/bin/bash
-# wrapper around ord to save log files in json to aws
-# simplifies mult-file inscription processing
-# perhaps we add functionality to launch in subshells and trigger something
-# when the inscription is confirmed? 
+# wrapper around ord to save log files in json and optionally upload to aws S3
+# this simplifies mult-file inscription processing, and creates a more complete
+# JSON log which includes filename and an optional inscription description / collection name
+# this provides additional data over the 'ord wallet inscription' command for future processing
 
+# REQUIREMENTS: 
+# a fully functional ord wallet - ie: 'ord wallet balance' displays a value
+# jq must be installed on the host for proper logging
 
+# OPTIONAL ENVIRONMENT VARIABLES:
 # manually create a .env file with your variables for the following
-# if these values are not defined we will not upload the inscribe_log to AWS S3
-# the inscribe_log will still be maintained on the host running this script
-# CLOUDFRONT_ID
+# CLOUDFRONT_ID=<your cloudfront ID>
 # aws_s3_uri=s3://hydren.io
 # aws_s3_dir=inscribed
+# note: if these values are not defined we will not upload the inscribe_log to AWS S3
+# the inscribe_log will still be maintained on the host running this script
 
-get_unconfirmed_trx(){
-    ord --wallet $wallet_name ${ord_args} wallet transactions | grep -E '\s0$' | awk '{ print $1 }' | uniq
-}
+# Depending on your bitcoind and ord configuration you may also need to define these:
+# if you can run 'ord' from any directory then the defaults will work properly.
+# Example: 
+# bitcoin_cli_args='-datadir=/var/lib/bitcoind'
+# ORD_ARGS='--bitcoin-data-dir /var/lib/bitcoind/ --rpc-url http://127.0.0.1:8332/wallet/ord --cookie-file /var/lib/bitcoind/.cookie'
+# BITCOIN_CLI_ARGS='-datadir=/var/lib/bitcoind'
 
-#for i in $(ord wallet transactions | grep -E '\s0$' | awk '{ print $1 }' | uniq); do bitcoin-cli getrawtransaction "$i"; done
-# check aws access key: aws configure get aws_access_key_id
+# perhaps we add functionality to launch in subshells and trigger something
+# when the inscription is confirmed on chain? This will allow us to iterate through
+# a larger group of files in an automated way as funds are deposited back into the wallet
 
-get_fee_rates(){
-    # checking for 1440 minutes / 24hr
-    # see: https://bitcoiner.live/doc/api
-    fee_rate_1440=$(curl -s https://bitcoiner.live/api/fees/estimates/latest | jq '.estimates."1440".sat_per_vbyte')
-    fee_rate_120=$(curl -s https://bitcoiner.live/api/fees/estimates/latest | jq '.estimates."120".sat_per_vbyte')
-    btc_price_usd=$(curl -s 'https://api.coindesk.com/v1/bpi/currentprice/USD.json' | jq '.bpi.USD.rate_float')
-}
-
-display_fee_rates(){
-    echo " current fee rates:"
-    echo "   1440: $fee_rate_1440"
-    echo "   120:  $fee_rate_120"
-}
-
-check_confirmation(){
-    txid=$1
-    echo "Checking if transaction $txid is confirmed"
-    while true; do
-        sleep 60
-        is_confirmed=$(bitcoin-cli ${bitcoin_cli_args} getrawtransaction "$txid" 1)
-        if [[ $is_confirmed =~ "confirmations" ]]; then
-            echo "Transaction $txid is confirmed"
-            break
-        fi
-    done
-}
+source .env
 
 check_balance(){ 
     echo "checking wallet balance and syncing index if needed..."
@@ -60,12 +43,57 @@ check_bitcoin_cli_balance(){
     bcli_balance=$(bitcoin-cli ${bitcoin_cli_args} -getinfo | grep "Balance:" | awk '{print $2}')
 }
 
+check_confirmation(){
+    txid=$1
+    echo "Checking if transaction $txid is confirmed"
+    while true; do
+        sleep 60
+        is_confirmed=$(bitcoin-cli ${bitcoin_cli_args} getrawtransaction "$txid" 1)
+        if [[ $is_confirmed =~ "confirmations" ]]; then
+            echo "Transaction $txid is confirmed"
+            break
+        fi
+    done
+}
+
+close_json_file(){
+    echo "]" >> ${inscribe_log}
+    ## jq . ${inscribe_log} | sponge ${inscribe_log} # beautify - this requires sponge
+}
+
+display_fee_rates(){
+    echo " current fee rates:"
+    echo "   1440: $fee_rate_1440"
+    echo "   120:  $fee_rate_120"
+}
+
 fetch_json_log(){
     aws s3 cp "${aws_s3_uri}"/"${aws_s3_dir}"/${inscribe_log} .
 }
 
+get_aws_url(){
+    #potentially add a direct reference for the file
+    aws s3 presign "${aws_s3_uri}"/"${aws_s3_dir}"/${1} --expires-in 604800  #1 week
+} 
+
 get_count_of_inscriptions(){
     jq '.[] | .inscription'  ${inscribe_log} | wc -l
+}
+
+get_fee_rates(){
+    # checking for 1440 minutes / 24hr
+    # see: https://bitcoiner.live/doc/api
+    fee_rate_1440=$(curl -s https://bitcoiner.live/api/fees/estimates/latest | jq '.estimates."1440".sat_per_vbyte')
+    fee_rate_120=$(curl -s https://bitcoiner.live/api/fees/estimates/latest | jq '.estimates."120".sat_per_vbyte')
+    btc_price_usd=$(curl -s 'https://api.coindesk.com/v1/bpi/currentprice/USD.json' | jq '.bpi.USD.rate_float')
+}
+
+get_unconfirmed_trx(){
+    ord --wallet $wallet_name ${ord_args} wallet transactions | grep -E '\s0$' | awk '{ print $1 }' | uniq
+}
+
+invalidate_s3_file(){
+    aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths "{$1}"
 }
 
 prep_json_to_log(){
@@ -73,23 +101,9 @@ prep_json_to_log(){
     echo "," >> ${inscribe_log}
 }
 
-close_json_file(){
-    echo "]" >> ${inscribe_log}
-    ## jq . ${inscribe_log} | sponge ${inscribe_log} # beautify
-}
-
 send_file_to_aws(){  # ORIGINAL_NAME  TARGET_NAME
     aws s3 cp "${1}" "${aws_s3_uri}"/"${aws_s3_dir}"/${2:=$1}
 }
-
-invalidate_s3_file(){
-    aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths "{$1}"
-}
-
-get_aws_url(){
-    #potentially add a direct reference for the file
-    aws s3 presign "${aws_s3_uri}"/"${aws_s3_dir}"/${1} --expires-in 604800  #1 week
-} 
 
 usage(){
     echo "Usage: $0 [--description|-d] [--fee|-f] [--skip|-s] FILENAME"
@@ -108,11 +122,12 @@ usage(){
     exit 0
 }
 
+#for i in $(ord wallet transactions | grep -E '\s0$' | awk '{ print $1 }' | uniq); do bitcoin-cli getrawtransaction "$i"; done
+# check aws access key: aws configure get aws_access_key_id
+
 
 bitcoin_cli_args=${BITCOIN_CLI_ARGS}
-#bitcoin_cli_args='-datadir=/var/lib/bitcoind'
 ord_args=${ORD_ARGS} # Set ENV Var
-#ord_args='--bitcoin-data-dir /var/lib/bitcoind/ --rpc-url http://127.0.0.1:8332/wallet/ord --cookie-file /var/lib/bitcoind/.cookie'
 ord_version=$(ord --version | cut -d ' ' -f 2)
 wallet_name=ord
 tmp_file=tmp_out.txt
@@ -122,6 +137,8 @@ aws_s3_uri=s3://hydren.io
 aws_s3_dir=inscribed
 ord_description=""
 skipcheck=false
+ord_explorer_url=https://ordinals.com/inscription/
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 while [[ $1 =~ ^- ]]; do
     case $1 in
@@ -157,6 +174,7 @@ while [[ $1 =~ ^- ]]; do
     shift
 done
 
+command -v jq >/dev/null 2>&1 || { echo >&2 "jq is required but it's not installed.  Aborting."; exit 1; }
 
 get_fee_rates
 check_balance
@@ -164,6 +182,14 @@ check_bitcoin_cli_balance
 
 if [ $# -eq 0 ]; then
  usage
+fi
+
+if [ -n "${cloudfront_id}" ] && [ -n "${aws_s3_uri}" ] && [ -n "${aws_s3_dir}" ]; then
+   echo "$inscribe_log will be sent to AWS S3"
+   aws_upload=true
+else
+   echo "AWS S3 vars undefined - we will not upload $inscribe_log to AWS"
+   aws_upload=false
 fi
 
 cmdline_filename=$1
@@ -179,7 +205,7 @@ if [ -z "$ord_description" ]; then
   ord_description="$root_filename"
 fi
 
-mkdir "./done" 2> /dev/null
+mkdir "${script_dir}/done" 2> /dev/null
 
 
 display_fee_rates
@@ -193,38 +219,49 @@ echo "USD COST: $usd_cost"
 
 [ "$skipcheck" = true ] || read -p "Press enter to continue...";
 
-
+## Inscribe
 ord --wallet $wallet_name ${ord_args} wallet inscribe ${cmdline_filename} --fee-rate ${fee_rate} &> $tmp_file
 ord_success=$?
 
 if [[ ${ord_success} -eq 0 ]]; then
     confirmation=$(cat ${tmp_file}  | jq -r '.commit')
     inscription=$(cat ${tmp_file} | jq -r '.inscription')
-    inscr_url=https://ordinals.com/inscription/$inscription
+    inscr_url=${ord_explorer_url}/$inscription
     echo "Confirmation: http://mempool.space/tx/${confirmation}"
     
-    # check_confirmation ${confirmation}
-    send_file_to_aws "${cmdline_filename}" "${inscription}_${cmdline_filename}" && mv "${cmdline_filename}" ./done/${inscription}_${cmdline_filename}
-    aws_url=$(get_aws_url "${inscription}_${cmdline_filename}")
+    # check_confirmation ${confirmation} details here
+    
+    # previously we were getting a presigned URL from AWS to add to the JSON:
+    # aws_url=$(get_aws_url "${inscription}_${cmdline_filename}")
+    # jq --arg aws_url "$aws_url" '. + {"aws_url": $aws_url}' | \
+    
     if [ -f ${inscribe_log} ]; then
         echo "Appending to existing $inscribe_log in current directory"
+        prep_json_to_log # this assumes $inscribe_log already contains an array
     else
-       echo "Fetching log from aws"
-       fetch_json_log # download from aws to append
+        echo "Fetching log from aws"
+        if [ "$aws_upload" = true ]; then
+            fetch_json_log # download from aws to append
+            prep_json_to_log # this assumes $inscribe_log already conatins an array
+        else
+            echo "[" > $inscribe_log
     fi
-    prep_json_to_log   
+       
     time_now=$(date +"%Y%m%d_%H:%M")UTC
     status="inscribed-${time_now}"
     cat ${tmp_file} | jq --arg file "$cmdline_filename"  '. + {"filename": $file}' | \
         jq --arg fee_rate "$fee_rate" '. + {"fee_rate": $fee_rate}' | \
-        jq --arg aws_url "$aws_url" '. + {"aws_url": $aws_url}' | \
         jq --arg explorer "$inscr_url" '. + {"explorer": $explorer}' | \
         jq --arg description "$ord_description" '. + {"description": $description}' | \
         jq --arg filesize "$filesize" '. + {"filesize": $filesize}' | \
         jq --arg status "$status" '. + {"status": $status}' >> ${inscribe_log}
     close_json_file
-    send_file_to_aws "${inscribe_log}" "${inscribe_log}"
-    aws cloudfront create-invalidation --distribution-id "$CLOUDFRONT_ID" --paths /${aws_s3_dir}/${inscribe_log}
+    if [ "$aws_upload" = true ]; then
+        send_file_to_aws "${cmdline_filename}" "${inscription}_${cmdline_filename}"
+        send_file_to_aws "${inscribe_log}" "${inscribe_log}"
+        aws cloudfront create-invalidation --distribution-id "$CLOUDFRONT_ID" --paths /${aws_s3_dir}/${inscribe_log}
+    fi
+    mv "${cmdline_filename}" ${script_dir}/done/${inscription}_${cmdline_filename}
 else
     echo "Unsuccessful inscription!"
     echo "$(cat $tmp_file)"
