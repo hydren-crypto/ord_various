dc822a9aeb7220ae58697205415151689223e98d
Kevin Sitzes <ksitzes@amperecomputing.com>
2023-03-07 18:20:42 -0500

    chore: test scan on one trx to cleanup json output

--- blockscan.sh
+++ blockscan.sh
@@ -18,6 +18,7 @@ stamp_json=stamp.json
 blockhash=$(bitcoin-cli getblockhash 779652)
 currentblock=$(bitcoin-cli getblockcount)
 lastblock="779652"
+source ${script_dir}/.env 2> /dev/null
 
 # open JSON for editing
 if [ -f ${stamp_json} ]; then
@@ -25,6 +26,7 @@ if [ -f ${stamp_json} ]; then
     prep_json_to_log # this assumes $inscribe_log already contains an array
 else
     echo "[" > $stamp_json
+    newjson=true
 fi
 
 #while [ $lastblock -lt $currentblock ]; do
@@ -36,6 +38,7 @@ counter=0
 
 for txid in $txids
 do
+    txid=17686488353b65b128d19031240478ba50f1387d0ea7e5f188ea7fda78ea06f4
     cntrprty_data=$(curl -s https://xchain.io/api/tx/$txid)
     cntrprtydesc=$(echo $cntrprty_data | jq '.description?')
     cntrprtydesc="${cntrprtydesc//\"}"
@@ -48,7 +51,11 @@ do
         echo "Found a Counterparty Trx"
         if [[ "$cntrprtydesc" == *"stamp"* ]]; then
             echo "FOUND A STAMP"
-            echo "," >> $stamp_json
+            if [[ $newjson == true ]]; then
+                echo "," >> $stamp_json
+                newjson=false
+                prep_json_to_log
+            fi
             stampstring=$(echo $cntrprtydesc | sed -n 's/.*stamp:"\?\(.*\)".*/\1/p')
             cat <<EOF >> $stamp_json
             {
@@ -66,3 +73,4 @@ EOF
 done
 
 echo "Total Number of Transactions Scanned: $counter"
+echo "]" > $stamp_json
