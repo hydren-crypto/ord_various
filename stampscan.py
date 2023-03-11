dd667461070cd63708bd1fed055e43b97b7d0fd8
moonblink <kevin@moonblink.com>
2023-03-11 20:47:12 +0000

    chore: update function to handle the indexes properly

--- stampscan.py
+++ stampscan.py
@@ -2,32 +2,40 @@ import json
 import os
 import requests
 import subprocess
+import pprint
 from requests.auth import HTTPBasicAuth
 
 
 CNTRPRTY_PASSWORD = "rpc"
-blockcount = int(subprocess.check_output(['fednode', 'exec', 'bitcoin', 'bitcoin-cli', 'getblockcount']).decode('utf-8'))
+
+blockstart = 779652
+blockend = int(subprocess.check_output(['fednode', 'exec', 'bitcoin', 'bitcoin-cli', 'getblockcount']).decode('utf-8'))
 
 url = "http://localhost:4000/api/"
 headers = {'content-type': 'application/json'}
 auth = HTTPBasicAuth('rpc', CNTRPRTY_PASSWORD)
 
-def make_request(method, block_index):
+def get_flocks(block_indexes):
   payload = {
-    "method": method,
+    "method": "get_blocks",
     "params": {
-             "block_indexes": [block_index]
+             "block_indexes": block_indexes
               },
     "jsonrpc": "2.0",
     "id": 0
   }
   response = requests.post(url, data=json.dumps(payload), headers=headers, auth=auth)
   output = (response.text)
-  #print(response.text)
   dumps = json.dumps(output)
-  #print(dumps)
-  data = json.loads(dumps.encode('utf-8'))
+#  data = json.loads(dumps.encode('utf-8'))
+  data = json.loads(dumps)
   print(data)
+  #result = data["result"]
 
-for block_list in range(779652, blockcount):
- make_request("get_blocks",block_list)
+
+#get_flocks(list(range(779652, blockend)))
+
+blockrange = list(range(blockstart,blockend))
+
+for i in range(0, len(blockrange), 249):
+      get_flocks(blockrange[i:i+249])
