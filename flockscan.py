50edc2a1b5017caf30d29b133d223b299bd6f4a3
moonblink <kevin@moonblink.com>
2023-03-12 16:06:51 +0000

    chore: add aws upload

--- flockscan.py
+++ flockscan.py
@@ -1,10 +1,12 @@
 import json
+import base64
+import magic
 import os
 import requests
 import subprocess
 import pprint
+import boto3
 from requests.auth import HTTPBasicAuth
-
 
 CNTRPRTY_PASSWORD = "rpc"
 
@@ -15,6 +17,35 @@ blockrange = list(range(blockstart,blockend))
 url = "http://localhost:4000/api/"
 headers = {'content-type': 'application/json'}
 auth = HTTPBasicAuth('rpc', CNTRPRTY_PASSWORD)
+
+
+def convert_base64_to_file(base64_string, item):
+    binary_data = base64.b64decode(base64_string)
+    file_type = magic.from_buffer(binary_data, mime=True)
+    _, file_extension = file_type.split("/")
+    tx_hash = item.get("tx_hash")
+    with open(f"{tx_hash}.{file_extension}", "wb") as f:
+        f.write(binary_data)
+    return f"{tx_hash}.{file_extension}"
+
+def upload_file_to_s3_aws_cli(file_path, bucket_name, s3_path):
+    print(["aws", "s3", "cp", file_path, f"s3://{bucket_name}/{s3_path}"])
+    subprocess.run(["aws", "s3", "cp", file_path, f"s3://{bucket_name}/{s3_path}"])
+    return True
+
+def upload_file_to_s3(file_path, bucket_name, s3_path):
+    s3 = boto3.client('s3')
+    s3.upload_file(file_path, bucket_name, s3_path)
+    return True
+
+def convert_json_array_files(json_string_array, bucket_name, s3_path):
+    json_dict = json.loads(json_string_array)
+    for item in json_dict:
+        base64_string = item.get("stamp_base64")
+        file_path = convert_base64_to_file(base64_string, item)
+        upload_file_to_s3_aws_cli(file_path, bucket_name, s3_path)
+    return True
+
 
 def get_flocks(block_indexes):
   payload = {
@@ -37,10 +68,9 @@ def get_flocks(block_indexes):
       if message["category"] == "issuances":
         bindings = message["bindings"]
         description = json.loads(bindings)["description"]
-        if "stamp:" in description.lower():
-          
-          stamp_search = description.lower().split("stamp:")[1]
-          stamp_base64 = stamp_search.strip() if len(stamp_search) > 1 else None
+        if description.lower().find("stamp:") != -1:
+          stamp_search = description[description.lower().find("stamp:")+6:]
+          stamp_base64 = stamp_search.strip() if len(stamp_search) > 1 else None  
           bindings = json.loads(bindings)
           message["bindings"] = {
             "description": bindings["description"],
@@ -95,4 +125,8 @@ for i in range(0, len(blockrange), 249):
 
 # Print the combined list
 json_string = json.dumps(combined_list, indent=4)
+#print(json_string)
+
+convert_json_array_files(json_string,"hydren.io","stamps/")
+
 print(json_string)
