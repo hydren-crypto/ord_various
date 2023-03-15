5cca99de1fbe3eef5365cbb13f6bdb147ded59a6
moonblink <kevin@moonblink.com>
2023-03-15 01:05:54 +0000

    chore: fix invalidation url, and begin boto support

--- flockscan.py
+++ flockscan.py
@@ -15,6 +15,8 @@ from requests.auth import HTTPBasicAuth
 # this will exclude the initial tests with text in the string suck as stamp:"data:image/png;base64,[base64]" 
 # this exclusion / malformatting can be tested with stamps prior to block 779652
 
+aws_access_key_id = ""
+aws_secret_access_key = ""
 aws_cloudfront_distribution_id = ""
 cntrprty_user = "rpc"
 cntrprty_password = "rpc"
@@ -44,7 +46,9 @@ s3_client = boto3.client(
     's3',
     aws_access_key_id=aws_access_key_id,
     aws_secret_access_key=aws_secret_access_key
-    ) 
+    )
+
+
        
 def convert_base64_to_file(base64_string, item):
     binary_data = base64.b64decode(base64_string)
@@ -80,6 +84,10 @@ def upload_file_to_s3_aws_cli(file_path, bucket_name, s3_path):
     subprocess.run(["aws", "s3", "cp", file_path, f"s3://{bucket_name}/{s3_path}"], stdout=subprocess.DEVNULL)
     return True
 
+def upload_file_to_s3_boto3(file_path, bucket_name, s3_key, s3_client):
+    with open(file_path, 'rb') as f:
+        s3_client.upload_fileobj(f, bucket_name, s3_key)
+
 def convert_json_array_files(json_string_array, bucket_name, s3_path):
     json_dict = json.loads(json_string_array)
     for item in json_dict:
@@ -87,7 +95,6 @@ def convert_json_array_files(json_string_array, bucket_name, s3_path):
         file_path = convert_base64_to_file(base64_string, item)
         if upload_file_to_s3_aws_cli(file_path, bucket_name, s3_path):
             os.remove(file_path)
-        
 
     return json.dumps(json_dict)
 
@@ -109,24 +116,33 @@ def get_flocks(block_indexes):
     block_data = result[i]
     messages = block_data["_messages"]
     for message in messages:
-      if message["category"] == "issuances":
+      # in message index [9142396,9142291] there is no identifying difference in the category  or command ffield
+      # only the  lock status  changed from false to true. the simplest method is to only allow the first trx of the asseet in as a stamp
+      # this also means that if a user creates an asset with lock = fales and a blank description field. then they
+      # add a stamp: formatted description and change the lock status to true it would then be counted as a stamp.
+
+      if message["category"] == "issuances" and message["command"] == "insert":
         bindings = message["bindings"]
+        # and an iff bindings["status"] == "valid"  ?
         description = json.loads(bindings)["description"]
         if description.lower().find("stamp:") != -1:
           stamp_search = description[description.lower().find("stamp:")+6:]
           stamp_base64 = stamp_search.strip() if len(stamp_search) > 1 else None  
           bindings = json.loads(bindings)
-          message["bindings"] = {
-            "description": bindings["description"],
-            "tx_hash": bindings["tx_hash"],
-            "asset": bindings["asset"],
-            "asset_longname": bindings["asset_longname"],
-            "block_index": bindings["block_index"],
-            "status": bindings["status"],
-            "tx_index": bindings["tx_index"],
-            "stamp_base64": stamp_base64 # add this line                                                       
-          }
-          output_list.append(message)
+          asset = bindings["asset"]
+          # Check if the asset already exists in the output_list
+          if not any(item["bindings"]["asset"] == asset for item in output_list):
+             message["bindings"] = {
+               "description": bindings["description"],
+               "tx_hash": bindings["tx_hash"],
+               "asset": bindings["asset"],
+               "asset_longname": bindings["asset_longname"],
+               "block_index": bindings["block_index"],
+               "status": bindings["status"],
+               "tx_index": bindings["tx_index"],
+               "stamp_base64": stamp_base64 # add this line                                                       
+             }
+             output_list.append(message)
 
   # Sort the list by message_index
   sorted_list = sorted(output_list, key=lambda k: k['message_index'])
@@ -159,6 +175,7 @@ def get_flocks(block_indexes):
 
   return flattened_list # Return the flattened list
 
+
 combined_list = []
 for i in range(0, len(blockrange), 249):
     combined_list += get_flocks(blockrange[i:i+249])
@@ -176,8 +193,22 @@ print(final_array_with_url)
 with open(json_output, 'w') as f:
     f.write(final_array_with_url)
 
+if aws_secret_access_key != "" and aws_acccess_key_id != "":
+# pending check for existing file list, we will not upload if it exists
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+        )
+    s3_objects = get_s3_objects(bucket_name, s3_client)
+    # s3_key should be == to stamps/txid.png
+    if s3_key not in s3_objects:
+        print(f'Uploading {local_file_path} to {s3_key}')
+        upload_file_to_s3(local_file_path, bucket_name, s3_key, s3_client)
+
+
 if aws_s3_bucketname != "" and aws_s3_dir != "" and aws_cloudfront_distribution_id != "":
     upload_file_to_s3_aws_cli(json_output,aws_s3_bucketname,"")
     # can purge local file upon successful upload
     # os.remove(json_output)
-    invalidate_s3_file("/" + aws_s3_dir + json_output)
+    invalidate_s3_file("/" + json_output)
