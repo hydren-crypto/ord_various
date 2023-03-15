e29af8a728d55239d64a5e3b64b68666d41636b7
Kevin Sitzes <ksitzes@amperecomputing.com>
2023-03-15 12:34:50 -0500

    chore: boto3 start

--- flockscan.py
+++ flockscan.py
@@ -1,5 +1,3 @@
-
-
 import json
 import time
 import base64
@@ -21,6 +19,7 @@ aws_cloudfront_distribution_id = ""
 cntrprty_user = "rpc"
 cntrprty_password = "rpc"
 cntrprty_api_url = "http://api.counterparty.io:4000/api/"
+diskless = False # if True, will not save stamps to disk
 
 # import private vars, may over-ride the above
 if os.path.exists('private_vars.py'):
@@ -29,6 +28,12 @@ if os.path.exists('private_vars.py'):
 # if the aws_cloudfront_distribution_id is not set these will be ignored
 aws_s3_bucketname = "stampchain.io"
 aws_s3_dir = "stamps/"
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key
+    )
+
 
 # saved in script dir
 json_output = "stamp.json"
@@ -59,8 +64,14 @@ def convert_base64_to_file(base64_string, item):
     _, file_extension = file_type.split("/")
     tx_hash = item.get("tx_hash")
     filename = f"{tx_hash}.{file_extension}"
-    with open(filename, "wb") as f:
-        f.write(binary_data)
+    if diskless:
+        # write the file directly to s3
+        print("writing to s3")
+    else:
+        # write the file to disk - update to send data_stream to s3 directly
+        with open(filename, "wb") as f:
+            f.write(binary_data)
+    # save the url back to the array
     item["stamp_url"] = "https://" + aws_s3_bucketname + "/" + aws_s3_dir  + filename
     return filename
 
@@ -80,15 +91,29 @@ def invalidate_s3_file(file_path):
     command = ["aws", "cloudfront", "create-invalidation", "--distribution-id", aws_cloudfront_distribution_id, "--paths", file_path]
     subprocess.run(command, stdout=subprocess.DEVNULL)
 
-def upload_file_to_s3_aws_cli(file_path, bucket_name, s3_path):
-    subprocess.run(["aws", "s3", "cp", file_path, f"s3://{bucket_name}/{s3_path}"], stdout=subprocess.DEVNULL)
+def upload_file_to_s3_aws_cli(local_file_path, bucket_name, s3_path):
+    subprocess.run(["aws", "s3", "cp", local_file_path, f"s3://{bucket_name}/{s3_path}"], stdout=subprocess.DEVNULL)
     return True
 
-def upload_file_to_s3_boto3(file_path, bucket_name, s3_key, s3_client):
-    with open(file_path, 'rb') as f:
+def upload_file_to_s3_boto3(local_file_path, bucket_name, s3_key, s3_client):
+    with open(local_file_path, 'rb') as f:
         s3_client.upload_fileobj(f, bucket_name, s3_key)
 
-def convert_json_array_files(json_string_array, bucket_name, s3_path):
+def upload_raw_data_to_s3(data_stream, bucket_name, target_fileloc_name):
+    # this is for aws lambda since it can't write to disk
+    # target_fileloc_name is the complete path and filename in s3
+    s3_client.upload_fileobj(data_stream, bucket_name, target_fileloc_name)
+    # # example usage:
+    # # Create an in-memory stream
+    # data_stream = io.BytesIO()
+    # # Write the data array to the stream
+    # data_stream.write(json.dumps(json_output))
+    # # Reset the stream position
+    # data_stream.seek(0)
+    # # Pass the filename and stream to the upload_file_to_s3 function
+    # upload_raw_data_to_s3(data_stream, bucket_name,target_fileloc_name, )
+
+def parse_json_array_convert_base64_to_file_and_upload(json_string_array, bucket_name, s3_path):
     json_dict = json.loads(json_string_array)
     for item in json_dict:
         base64_string = item.get("stamp_base64")
@@ -187,28 +212,25 @@ for message in combined_list:
 
 
 json_string = json.dumps(combined_list, indent=4)
-final_array_with_url=(convert_json_array_files(json_string,aws_s3_bucketname,aws_s3_dir))
+final_array_with_url=(parse_json_array_convert_base64_to_file_and_upload(json_string,aws_s3_bucketname,aws_s3_dir))
 print(final_array_with_url)
+
 
 with open(json_output, 'w') as f:
     f.write(final_array_with_url)
 
-if aws_secret_access_key != "" and aws_acccess_key_id != "":
+if aws_secret_access_key != "" and aws_access_key_id != "":
 # pending check for existing file list, we will not upload if it exists
-    s3_client = boto3.client(
-        's3',
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key
-        )
-    s3_objects = get_s3_objects(bucket_name, s3_client)
+    s3_objects = get_s3_objects(aws_s3_bucketname, s3_client)
     # s3_key should be == to stamps/txid.png
     if s3_key not in s3_objects:
         print(f'Uploading {local_file_path} to {s3_key}')
-        upload_file_to_s3(local_file_path, bucket_name, s3_key, s3_client)
+        upload_file_to_s3_boto3(local_file_path, bucket_name, s3_key, s3_client)
 
 
 if aws_s3_bucketname != "" and aws_s3_dir != "" and aws_cloudfront_distribution_id != "":
-    upload_file_to_s3_aws_cli(json_output,aws_s3_bucketname,"")
+    # upload_file_to_s3_aws_cli(json_output,aws_s3_bucketname,"")
+    upload_file_to_s3_boto3(json_output,aws_s3_bucketname,aws_s3_dir,s3_client)
     # can purge local file upon successful upload
     # os.remove(json_output)
     invalidate_s3_file("/" + json_output)
